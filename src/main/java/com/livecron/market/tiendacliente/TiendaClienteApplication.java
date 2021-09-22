2ca63cf8e3ce9387b71e502ddb140f70d00c0ba6
Henry Zerda Rodriguez <henry.zrz@gmail.com>
2021-09-22 00:01:09 -0400

    commit inicial con el proyecto creado

---
+++ src/main/java/com/livecron/market/tiendacliente/TiendaClienteApplication.java
@@ -0,0 +1,13 @@
+package com.livecron.market.tiendacliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livecron/market/tiendacliente/TiendaClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livecron.market.tiendacliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
